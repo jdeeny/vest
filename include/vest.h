2c80f824f9c7c5998cc334ea8f0f9fc9d0c9b8d0
jdeeny <jdeeny@gmail.com>
2016-09-27 19:44:20 -0600

    Cleanup interface a bit

--- include/vest.h
+++ include/vest.h
@@ -1,24 +1,31 @@
 #include <stdint.h>
 
-typedef struct VEST_VelocityEstimator* VEST_VelocityEstimator;
-
-// Type used for counting element numbers and indexes within the data structure
 typedef uint32_t VEST_USize;
 typedef uint32_t VEST_Time;
 typedef uint32_t VEST_Data;
 
+typedef struct VEST_Estimator* VEST_Estimator;
+
+struct VEST_Config {
+  VEST_USize window_size;
+  VEST_Time period;
+  VEST_USize window_min;
+};
+
+typedef struct VEST_Config VEST_Config;
+
 typedef enum {
-  OK,
+  VEST_OK,
 } VEST_Result;
 
-// Allocate memory for a VEST_VelocityEstimator
-VEST_VelocityEstimator VEST_alloc();
+// Allocate memory for a VEST_Estimator
+VEST_Estimator VEST_alloc(VEST_USize window_size);
 
 // Initializes a VEST_VelocityEstimator
-VEST_Result VEST_init(VEST_VelocityEstimator state, VEST_USize window_size, VEST_Time period);
+VEST_Result VEST_init(VEST_Estimator state, VEST_Config config);
 
 // Push an element into the window, updating state as required
-VEST_Result VEST_push(VEST_VelocityEstimator state, VEST_Data data);
+VEST_Result VEST_push(VEST_Estimator state, VEST_Data data);
 
 // Return the current velocity estimate, by performing calculations on state
-VEST_Result VEST_get_velocity(VEST_VelocityEstimator state, VEST_Data* result);
+VEST_Result VEST_get_velocity(VEST_Estimator state, VEST_Data* result);
--- ref/velocity.c
+++ ref/velocity.c
@@ -1,1 +1,33 @@
 // The naive implementation
+
+#include "vest.h"
+#include <stdlib.h>
+
+struct VEST_Estimator {
+  VEST_Config config;
+  VEST_Data data[];
+};
+
+// Return the number of bytes that will be used for an estimator
+static VEST_USize _VEST_bytes(VEST_USize window_size) {
+  VEST_USize header_size = sizeof(VEST_Config) + sizeof(VEST_USize) * 2;
+  VEST_USize data_size = sizeof(VEST_Data) * window_size;
+  return header_size + data_size;
+}
+
+VEST_Estimator VEST_alloc(VEST_USize window_size) {
+  return malloc( _VEST_bytes(window_size) );
+}
+
+VEST_Result VEST_init(VEST_Estimator state, VEST_Config config) {
+  state->config = config;
+}
+
+VEST_Result VEST_push(VEST_Estimator state, VEST_Data data) {
+  return VEST_OK;
+}
+
+VEST_Result VEST_get_velocity(VEST_Estimator state, VEST_Data* result) {
+  *result = 0;
+  return VEST_OK;
+}
